0e13033399d32a19fd79be8bed0f43bd659111db
Murari <73845678+murari0@users.noreply.github.com>
2023-02-04 13:55:55 +0000

    Kernel functions
    Signed-off-by: Murari <73845678+murari0@users.noreply.github.com>

---
+++ src/kernels.jl
@@ -0,0 +1,21 @@
+abstract type KernelFunction end
+
+struct InversionRecovery end
+struct SaturationRecovery end
+struct TransverseRelaxation end
+
+const t1ir = InversionRecovery
+const t1sr = SaturationRecovery
+const t2 = TransverseRelaxation
+
+function (k::t1ir)(t<:Real, s<:Real)
+    1.0 - 2.0*exp(-t*s)
+end
+
+function (k::t1sr)(t<:Real, s<:Real)
+    1.0 - exp(-t*s)
+end
+
+function (k::t2)(t<:Real, s<:Real)
+    exp(-t*s)
+end
