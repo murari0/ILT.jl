809f19c244f0ae2b4ae564655a957cc69d0a6756
Murari Soundararajan <73845678+murari0@users.noreply.github.com>
2023-06-14 06:42:18 +0100

    Add docstrings

--- src/ILT.jl
+++ src/ILT.jl
@@ -12,6 +12,21 @@ export
     read_bruker_p2d,
     read_2d!
 
+"""
+    ilt(t, y, logr_min, logr_max, N[, solveropts...]; kwargs...)
+    ilt(t, y, r, N[, solveropts...]; kwargs...)
+
+Computes the inverse Laplace transform of `y(t)`
+
+Sets up an optimisation problem to invert the Laplace transform of the function defined by the arguments `t` and `y`. The vector `r` (as given by the user or generated as a logarithmically spaced range from parameters) is the set of points in r-space at which the inverse is calculated. The optimisation problem is solved using Ipopt.
+
+`solveropts` is a list of optional arguments specified as `"key"=>"value"` pairs that are passed to Ipopt. For example, the key `"linear_solver"` tells Ipopt what linear solver to use - specifying `"linear_solver"=>"ma27"` will instruct Ipopt to use the STFC's HSL_MA27 solver, if available.
+
+The output tuple `(r, F, residual)` contains the values `F` of the inverse at each point in `r`. Both vectors have length `N+1` - an additional point, `0.0` is pushed to the start of the set of relaxation rates to account for any residual y-offset in the data. The vectors `F[2:end]` and `r[2:end]` contain the solution at finite relaxation times/rates.
+
+# Keyword arguments
+- 
+"""
 function ilt(t, y, logr_min, logr_max, N, solveropts::Pair...; α = 1.0, fn::KernelFunction = t1ir())
     r = exp10.(range(logr_min,logr_max,length=N))
     ilt(t,y,r,solveropts...;α,fn)
